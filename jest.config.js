783f1efea54cfb380c3f9a7bfdb6e60a1e1348e0
Harendra <theekshanaharendra@gmail.com>
2024-01-10 18:01:42 +0530

    merge changes

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,3 @@
-// jest.config.js
 module.exports = {
-    preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/src/$1',
-    },
-    transformIgnorePatterns: ['/node_modules/(?!vue3-toastify)'],
+    preset: "@vue/cli-plugin-unit-jest"
   };
-  
